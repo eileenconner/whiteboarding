32277bf6ca98a457b9e66a333da1d49a8fe0ecff
Eileen Conner <eileenconner@gmail.com>
2015-12-29 09:58:16 -0800

    Organized files. Added print_recursively

---
+++ print-recursively/print_recursively.py
@@ -0,0 +1,22 @@
+"""Print items from a list using recursion.
+
+    >>> print_recursively([1, 2, 3])
+    1
+    2
+    3
+
+"""
+
+
+def print_recursively(lst):
+    """Print items from a list using recursion."""
+
+    if lst:
+        print lst[0]
+        print_recursively(lst[1:])
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. WIN FOR YOU!\n"
