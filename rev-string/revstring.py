bb87925d933b369f28520c07d066817ab5ff358b
Eileen Conner <eileenconner@gmail.com>
2016-02-05 16:20:41 -0800

    Added rev-string/

---
+++ rev-string/revstring.py
@@ -0,0 +1,28 @@
+"""Reverse a string.
+
+For example::
+
+    >>> rev_string("")
+    ''
+
+    >>> rev_string("a")
+    'a'
+
+    >>> rev_string("porcupine")
+    'enipucrop'
+
+"""
+
+
+def rev_string(astring):
+    """Return reverse of string.
+
+    You may NOT use the reversed() function!
+    """
+    return astring[::-1]
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. !KROW DOOG\n"
